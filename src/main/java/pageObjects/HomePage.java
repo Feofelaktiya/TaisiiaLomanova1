6077ea309ff18600a2e72d89b2838a328927cf5e
Feofelaktiya <kxc$#2FV>
2019-03-25 03:53:35 +0300

    sm changes1

--- src/main/java/pageObjects/HomePage.java
+++ src/main/java/pageObjects/HomePage.java
@@ -1,5 +1,6 @@
 package pageObjects;
 
+import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
